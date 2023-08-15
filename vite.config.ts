d16820ed2226194477bd026300435b3c99a7d20d
Andrey <zlobin.andy@gmail.com>
2023-08-15 10:03:35 +0300

    ci test

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+console.log('ci-test')
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
